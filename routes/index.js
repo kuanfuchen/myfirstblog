6a528872c7296731fdbb41f85d87f531d8c5986b
kuanfuchen <creazy_fu@hotmail.com>
2019-01-10 23:53:44 +0800

    update5

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/post', function (req, res, next) {
+  res.render('post', {
+    title: 'Express'
+  });
+});
+
+router.get('/dashboard/article', function (req, res, next) {
+  // 上方為寫入js
+  res.render('dashboard/article', {
+    // 上方為寫入ejs
+    title: 'Express'
+  });
+});
+router.get('/dashboard/archives', function (req, res, next) {
+  res.render('dashboard/archives', {
+    title: 'Express'
+  });
+});
+router.get('/dashboard/categories', function (req, res, next) {
+  res.render('dashboard/categories', {
+    title: 'Express'
+  });
+});
+router.get('/dashboard/signup', function (req, res, next) {
+  res.render('dashboard/signup', {
+    title: 'Express'
+  });
+});
+module.exports = router;
